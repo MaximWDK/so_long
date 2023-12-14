d95b001663524ba0dcb397c8c5d20f6a14fb1a66
mleonet <mleonet@student.s19.be>
2023-12-14 10:39:14 +0100

    Protected strlen

--- libft/ft_strlen.c
+++ libft/ft_strlen.c
@@ -6,7 +6,7 @@
 /*   By: mleonet <mleonet@student.s19.be>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/24 01:25:19 by mleonet           #+#    #+#             */
-/*   Updated: 2023/05/09 14:15:30 by mleonet          ###   ########.fr       */
+/*   Updated: 2023/12/14 10:36:13 by mleonet          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,8 @@ size_t	ft_strlen(const char *str)
 	size_t	i;
 
 	i = 0;
+	if (!str)
+		return (0);
 	while (str[i])
 		i++;
 	return (i);
